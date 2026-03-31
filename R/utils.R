4c42a202c7a9494e51a4528d2bc9e33cbaa6949c
Stevejb1 <stevejb904@icloud.com>
2026-03-31 14:28:05 -0400

    Prepare for CRAN submission v0.1.0

--- R/utils.R
+++ R/utils.R
@@ -1,11 +1,15 @@
-#' gpr package
+#' gpr: General Power for R
 #'
-#' @docType package
 #' @name gpr-package
-#' @importFrom stats dbinom dchisq df dnorm dt pbinom pchisq pf pnorm pt qbinom qchisq qf qnorm qt uniroot weighted.mean
-#' @importFrom ggplot2 ggplot aes geom_line geom_area geom_col geom_vline geom_hline annotate scale_y_continuous scale_color_manual scale_fill_manual labs theme_minimal theme element_text element_blank
+#' @aliases gpr-package
+#' @importFrom stats dbinom dchisq df dnorm dt pbinom pchisq pf pnorm
+#'   pt qbinom qchisq qf qnorm qt uniroot weighted.mean
+#' @importFrom ggplot2 ggplot aes geom_line geom_area geom_col
+#'   geom_vline geom_hline annotate scale_y_continuous
+#'   scale_color_manual scale_fill_manual labs theme_minimal theme
+#'   element_text element_blank stat_function
 #' @importFrom scales percent_format
+#' @importFrom shiny req
 NULL
 
-# Suppress R CMD check notes for ggplot2 column names
-utils::globalVariables(c('density', 'distribution', 'dist', 'prob'))
+utils::globalVariables(c('density', 'distribution', 'dist', 'prob', 'y', 'x'))
